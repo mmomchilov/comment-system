c8334b0ee3e2ab2e3da8444f4ec7d388ba40e77e
mmomchilov <mmomchilov@csc.com>
2019-05-30 10:37:52 +0300

    Update html of firstPage

--- src/app/firstPage/firstPage.module.ts
+++ src/app/firstPage/firstPage.module.ts
@@ -1,14 +1,8 @@
-// import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
-// import { FirstPageComponent } from './firstPage.component';
+import { NgModule } from '@angular/core';
+import { FirstPageComponent } from './firstPage.component';
 
-
-const routes: Routes = [];
-
-// @NgModule({
-//   imports: [
-
-//   ],
-//   declarations: [FirstPageComponent]
-// })
-// export class FirstPageModule { }
+@NgModule({
+    imports: [],
+    declarations: [FirstPageComponent]
+})
+export class FirstPageModule { }
