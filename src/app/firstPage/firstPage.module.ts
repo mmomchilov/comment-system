0ff7a5950310f550caf31292b97c5217d86b6f46
mmomchilov <mmomchilov@csc.com>
2019-05-30 14:53:20 +0300

    dbg

--- src/app/firstPage/firstPage.module.ts
+++ src/app/firstPage/firstPage.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FirstPageComponent } from './firstPage.component';
-import { CommentServiceModule } from './firstPage.component';
-
 
 @NgModule({
-    imports: [CommentServiceModule],
     declarations: [FirstPageComponent]
 })
 export class FirstPageModule { }
